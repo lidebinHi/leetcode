699b43f538960ee5f8774b4253a9d34bc4a020d1
lidebin <lidebin.hi@gmail.com>
2023-04-24 22:40:34 +0800

    环形链表

---
+++ 4.24 环形链表.js
@@ -0,0 +1,24 @@
+// 给你一个链表的头节点 head ，判断链表中是否有环。
+// 如果链表中有某个节点，可以通过连续跟踪 next 指针再次到达，则链表中存在环。
+// 为了表示给定链表中的环，评测系统内部使用整数 pos 来表示链表尾连接到链表中的位置（索引从 0 开始）。注意：pos 不作为参数进行传递 。仅仅是为了标识链表的实际情况。
+function ListNode(val) {
+    this.val = val;
+    this.next = null;
+}
+
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+const hasCycle = (head) => {
+    if(!head) return false
+    const map = new Map()
+    while (head) {
+        if(map.has(head)){
+            return true
+        }
+        map.set(head, null)
+        head = head.next
+    }
+    return false
+};
